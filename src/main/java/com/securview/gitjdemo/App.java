378c87106d67fea3055b504006476dc1a1ff4d5f
nlshpwr99 <nlshpwr99@gmail.com>
2021-08-14 21:37:45 +0530

    SKS-14111 master updated 4

--- src/main/java/com/securview/gitjdemo/App.java
+++ src/main/java/com/securview/gitjdemo/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World 2 !" );
         System.out.println( "Hello World 2 !" );
         System.out.println( "Jira - 1" );
+        System.out.println( "master updated4" );
 
     }
     
