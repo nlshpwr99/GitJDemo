07a39f03648aca96222f9e17a83a52c805117498
nlshpwr99 <nlshpwr99@gmail.com>
2021-08-14 23:57:27 +0530

    Master - update-1

--- src/main/java/com/securview/gitjdemo/App.java
+++ src/main/java/com/securview/gitjdemo/App.java
@@ -9,16 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World 2 !" );
-        System.out.println( "Hello World 2 !" );
-        System.out.println( "Jira - 1" );
-        System.out.println( "master updated4" );
+       
 
     }
     
     private static void newMethod() {
     	System.out.println( "newMethod" );
-    	System.out.println( "master updated" );
-    	System.out.println( "master updated2" );
-    	System.out.println( "master updated3" );
+    	
     }
 }
