759af864a44e39ae059b0222fbc187300c4229cd
nlshpwr99 <38489480+nlshpwr99@users.noreply.github.com>
2021-08-14 21:24:43 +0530

    Jira- 1 - updated11 (#2)
    first subtask done

--- src/main/java/com/securview/gitjdemo/App.java
+++ src/main/java/com/securview/gitjdemo/App.java
@@ -11,13 +11,7 @@ public class App
         System.out.println( "Hello World 2 !" );
         System.out.println( "Hello World 2 !" );
         System.out.println( "Jira - 1" );
-<<<<<<< Upstream, based on branch 'feature/Jira_Integration' of git@github.com:nlshpwr99/GitJDemo.git
-        System.out.println( "Jira- 1 - updated11" );
-        System.out.println( "master updated4" );
-=======
-        System.out.println( "master updated4" );
 
->>>>>>> 378c871 SKS-14111 master updated 4
     }
     
     private static void newMethod() {
