c5d282e52e43073bf0aabf8342cdddc43c99503a
nlshpwr99 <38489480+nlshpwr99@users.noreply.github.com>
2021-08-15 10:28:35 +0530

    New feature (#3)
    * new-feature update-1
    
    * new-feature-last-commit

--- src/main/java/com/securview/gitjdemo/App.java
+++ src/main/java/com/securview/gitjdemo/App.java
@@ -8,13 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World 2 !" );
         System.out.println( "new change" );
-
+        System.out.println( "Hello World 22 !" );
+        System.out.println( "New changes by feature" );
     }
     
     private static void newMethod() {
     	System.out.println( "newMethod" );
-    	
+        System.out.println( "Hello World 22 !" );
     }
+
 }
